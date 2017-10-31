e818da66269e22e9b6f9cd880332e4a03ba7b52c
Ahmed ABERWAG <aberwag@gmail.com>
2017-10-31 23:33:13 +0100

    add swagger

--- src/main/java/fr/aberwag/family/FamilyTreeApplication.java
+++ src/main/java/fr/aberwag/family/FamilyTreeApplication.java
@@ -3,12 +3,36 @@ package fr.aberwag.family;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EntityScan("fr.aberwag.family.domain")
+@EnableSwagger2
 public class FamilyTreeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FamilyTreeApplication.class, args);
 	}
+
+	@Bean
+	public Docket swaggerSpringMvcPlugin() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.apiInfo(apiInfo())
+				.select()
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Family tree")
+				.description("Rest API or Family tree")
+				.build();
+	}
+
 }
